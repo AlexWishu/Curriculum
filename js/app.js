784c7ca3c3b9921c959cad1703104e70533f0499
Alexander Perez Urbina <alexander.perez.urbina55@gmail.com>
2021-01-07 18:10:32 -0300

    Aqui agregamos fontawesome, un anchor que redirige hasta el tope de la pagina con una funcion que hace que aparesca y desaparesca segun el scroll, y una seccion llamada informacion en donde agrego informacion importante mia

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
+        
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
+});
+
+$(document).ready(() => {
+    $('#btn-scroll-up').hide();
+})
+
+$(window).scroll(function (event) {
+    let scroll = $(window).scrollTop();
+    if(scroll > 408){
+        $('#btn-scroll-up').fadeIn(300);
+    } else {
+        $('#btn-scroll-up').fadeOut(200);
+    }
 });
